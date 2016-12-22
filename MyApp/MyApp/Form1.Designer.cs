5e4ae910454817c91577ae1f0c27fc7a6c24db1a
Francis <djfhjd@djfhjdf.com>
2016-12-22 01:26:03 +0000

    Added MyApp Windows Version
    Added the files for the Windows Version

---
+++ MyApp/MyApp/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace MyApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.unlockBox = new System.Windows.Forms.TextBox();
+            this.unlockButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusLabel.Location = new System.Drawing.Point(327, 111);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(61, 20);
+            this.statusLabel.TabIndex = 0;
+            this.statusLabel.Text = "Locked";
+            // 
+            // unlockBox
+            // 
+            this.unlockBox.Location = new System.Drawing.Point(180, 150);
+            this.unlockBox.Name = "unlockBox";
+            this.unlockBox.Size = new System.Drawing.Size(352, 20);
+            this.unlockBox.TabIndex = 1;
+            // 
+            // unlockButton
+            // 
+            this.unlockButton.Location = new System.Drawing.Point(311, 188);
+            this.unlockButton.Name = "unlockButton";
+            this.unlockButton.Size = new System.Drawing.Size(88, 23);
+            this.unlockButton.TabIndex = 2;
+            this.unlockButton.Text = "Unlock";
+            this.unlockButton.UseVisualStyleBackColor = true;
+            this.unlockButton.Click += new System.EventHandler(this.unlockButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(728, 353);
+            this.Controls.Add(this.unlockButton);
+            this.Controls.Add(this.unlockBox);
+            this.Controls.Add(this.statusLabel);
+            this.Name = "Form1";
+            this.Text = "My App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.TextBox unlockBox;
+        private System.Windows.Forms.Button unlockButton;
+    }
+}
+
---
+++ MyApp/MyApp/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyApp
+{
+    public partial class Form1 : Form
+    {
+
+        private String[] unlockCodes = { "A1-B2-C3", "D2-E3-F4", "X1-Y2-Z3" };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void unlockApp()
+        {
+            foreach (String unlockCode in unlockCodes) {
+
+                if (unlockBox.Text == unlockCode)
+                {
+                    statusLabel.Text = "Unlocked!";
+                    unlockBox.Enabled = false;
+                    unlockButton.Enabled = false;
+                    return;
+                }
+
+            }
+
+             MessageBox.Show("The unlock code you supplied is not valid!", "Invalid Code");
+        }
+
+        private void unlockButton_Click(object sender, EventArgs e)
+        {
+
+            unlockApp();
+
+        }
+    }
+}
